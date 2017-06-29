f0af545cd58e67ba49c0182e1ef25ea880cb4736
Dominik-Hillmann <dominik.hillmann@gmx.de>
2017-06-29 19:26:19 +0200

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,11 +1,32 @@
-var pointContainer = [];
+// contains connection points for the lines and the methods they need
+var pointContainer =
+{
+   container : []
+};
 
+// draws line between two points in pointContainer.container if they have less than a certain distance to each other
+pointContainer.display = function()
+{
+   for(var i = 0; i < this.container.length; i++)
+   {
+      for(var j = 0; j < this.container.length; j++)
+      {
+         if(isWithinDistance(this.container[i], this.container[j], 200) && (i != j))
+         {
+            line(this.container[i].x, this.container[i].y, this.container[j].x, this.container[j].y);
+         }
+      }
+   }
+}
+
+// constructor for a point on the canvas
 var ConnectionPoint = function(x, y)
 {
    this.x = x;
    this.y = y;
 }
 
+// checks whether two points are within the minDistance of each other
 var isWithinDistance = function(point1, point2, minDistance)
 {
    if(Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2)) <= minDistance)
--- sketch.js
+++ sketch.js
@@ -1,72 +1,49 @@
-const WIDTH = 400;
-const HEIGHT = 400;
-const POINT_SIZE = 5;
-const NUMBER_POINTS = 20; // number of start points, use .length later because maybe it should be able to change
+const WIDTH = 700;
+const HEIGHT = 700;
+const NUMBER_POINTS = 60; // # of points that will be on the canvas
 
 function setup()
 {
    createCanvas(WIDTH, HEIGHT);
-   // first points at random location on canvas
+   // first initialize points at random location on canvas
    for(var i = 0; i < NUMBER_POINTS; i++)
    {
-      pointContainer.push(new ConnectionPoint
+      pointContainer.container.push(new ConnectionPoint
       (
          Math.round(random(WIDTH)),
          Math.round(random(HEIGHT))
       ));
-      console.log(pointContainer[i]);
    }
 }
 
 function draw()
 {
-   background(0, 0, 0, 5);
+   background(0, 0, 0);
    stroke(255, 255, 255);
-   for(var i = 0; i < pointContainer.length; i++)
-   {
-      for(var j = 0; j < pointContainer.length; j++)
-      {
-         if(isWithinDistance(pointContainer[i], pointContainer[j], 200) && (i != j))
-         {
-            line(pointContainer[i].x, pointContainer[i].y, pointContainer[j].x, pointContainer[j].y);
-         }
-      }
-   }
-
-   // changing positions of points with Perlin noise
+   pointContainer.display();
+   // changes position of every point with Perlin noise
    var xoff = 0;
-   for(var i = 0; i < pointContainer.length; i++)
+   for(var i = 0; i < pointContainer.container.length; i++)
    {
-      pointContainer[i].x += map(noise(xoff), 0, 1, -1, 1);
-      pointContainer[i].y += map(noise(xoff + 1000), 0, 1, -1, 1); // number that can be negative; in pixels
-      xoff += 0.2; //console.log(xoff);
+      pointContainer.container[i].x += map(noise(xoff), 0, 1, -1, 1);
+      pointContainer.container[i].y += map(noise(xoff + 1000), 0, 1, -1, 1);
+      xoff += 0.2;
 
-      if(pointContainer[i].x > (WIDTH + POINT_SIZE))
+      if(pointContainer.container[i].x > WIDTH)
       {
-         pointContainer[i].x = 0;
+         pointContainer.container[i].x = 0;
       }
-      if(pointContainer[i].y > (WIDTH + POINT_SIZE))
+      if(pointContainer.container[i].y > WIDTH)
       {
-         pointContainer[i].y = 0;
+         pointContainer.container[i].y = 0;
       }
-      if(pointContainer[i].x < (0 - POINT_SIZE))
+      if(pointContainer.container[i].x < 0)
       {
-         pointContainer[i].x = WIDTH;
+         pointContainer.container[i].x = WIDTH;
       }
-      if(pointContainer[i].y < (0 - POINT_SIZE))
+      if(pointContainer.container[i].y < 0)
       {
-         pointContainer[i].y = HEIGHT;
+         pointContainer.container[i].y = HEIGHT;
       }
    }
 }
-
-/* TODO
-   Array für die ganzen Punkte machen, mit den Objekten - CHANGED
-   Wie sich diese Punkte bewegen sollen (auch Verhalten am Rand!) - CHANGED
-   Linien zwischen ihnen zeichnen lassen - CHANGED
-   sagen, wann die Linien da sein sollen und wann nicht - CHANGED
-   das ganze schön machen (Farbverlauf, Dicke nach Nähe)
-   elegantere Lösung für Ränder finden
-*/
-
-// PS nicht vergessen, neue Files im HTML file zu erwähnen
